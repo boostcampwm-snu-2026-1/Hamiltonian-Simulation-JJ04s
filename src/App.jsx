4898110a7e54a59668759708a2930d240fabcda0
JJ04s <bykim704@gmail.com>
2026-06-03 14:49:32 +0900

    init: React+Vite 초기 프로젝트 구축 및 라이브러리 설치

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div>
+      <h1>Hamiltonian Simulation Workspace</h1>
+      <p>개발 환경 구축 완료. SimulationContext 구현을 시작하세요.</p>
+    </div>
+  )
+}
+
+export default App
